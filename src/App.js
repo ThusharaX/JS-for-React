3ee8e22bb07a23a70c51d5fb93830ead5fee509a
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2021-11-15 17:15:30 +0530

    App.js Update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+import Strings from "./Strings";
 import Variables from "./Variables";
 import Arrays from "./Arrays";
 import DestructuringAssignment from "./DestructuringAssignment";
 import DestructuringAssignmentReactUse from "./DestructuringAssignmentReactUse";
+import Conditions from "./Conditions";
+import Loops from "./Loops";
+import ErrorHandling from "./ErrorHandling";
+import Functions from "./Functions";
 
 function App() {
 
 	return (
 		<>
+			{/* <Strings /> */}
 			{/* <Variables /> */}
 			{/* <Arrays /> */}
 			{/* <DestructuringAssignment /> */}
-			<DestructuringAssignmentReactUse />
+			{/* <DestructuringAssignmentReactUse /> */}
+			{/* <Conditions /> */}
+			{/* <Loops /> */}
+			{/* <ErrorHandling /> */}
+			<Functions />
 		</>
 	);
 }
